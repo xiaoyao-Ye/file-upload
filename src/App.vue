12b7fe94133944bfb9c8d72485d4a02636ec3d68
xiaoyao-Ye <yqcyecong@163.com>
2023-03-12 23:50:31 +0800

    feat: upload chunk and merge chunk

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,14 @@
 </template>
 
 <script setup lang="ts">
+import SparkMD5 from 'spark-md5';
 import { ref } from 'vue';
-import { uploadFile } from './api/upload/api';
+import { mergeChunks, uploadFile } from './api/upload/api';
 
-const accept = '.jpg, .jpeg, .png';
+/** 分片大小 */
+const SIZE = 5 * 1024 * 1024;
+// const accept = '.jpg, .jpeg, .png';
+const accept = '';
 const multiple = false;
 const selectedFile = ref<File | null>(null);
 
@@ -21,10 +25,51 @@ const handleFileUpload = async (event: Event) => {
 const onSubmit = async () => {
   console.log('onSubmit')
   if (!selectedFile.value) return;
-  const fd = new FormData();
-  fd.append('file', selectedFile.value);
-  const res = await uploadFile(fd);
-  console.log('uploadFile', res);
+  const chunks = createFileChunk(selectedFile.value);
+  console.log('chunks', chunks);
+  const hash = await calculateHash(chunks);
+  console.log('hash', hash);
+  await uploadChunks(chunks, hash);
+  await mergeChunks({ fileHash: hash, fileName: selectedFile.value?.name ?? '' })
+}
+
+const createFileChunk = (file: File, size = SIZE) => {
+  const chunks: Blob[] = [];
+  let cur = 0;
+  while (cur < file.size) {
+    chunks.push(file.slice(cur, cur + size));
+    cur += size;
+  }
+  return chunks;
+}
+
+const calculateHash = async (chunks: Blob[]): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    const spark = new SparkMD5.ArrayBuffer();
+    chunks.forEach((chunk, index) => {
+      const fileReader = new FileReader();
+      fileReader.readAsArrayBuffer(chunk);
+      fileReader.onload = (e) => {
+        spark.append(e.target?.result as ArrayBuffer);
+      }
+    });
+    resolve(spark.end());
+  })
+}
+
+const uploadChunks = async (chunks: Blob[] = [], hash: string) => {
+  const requestList = chunks.map((chunk, index) => {
+    const formData = new FormData();
+    formData.append('chunk', chunk);
+    formData.append('hash', `${hash}-${index}`);
+    formData.append('fileName', selectedFile.value?.name ?? '');
+    // formData.append('fileHash', hash);
+    return { formData, index };
+  }).map(({ formData, index }) => {
+    return uploadFile(formData, index);
+  });
+  console.log('requestList', requestList);
+  await Promise.all(requestList);
 }
 
 </script>
--- src/api/upload/api.ts
+++ src/api/upload/api.ts
@@ -1,7 +1,21 @@
 import service from "..";
 
 /** 上传文件 */
-export const uploadFile = async (fd: FormData) => {
-  return await service.post('/upload', fd);
+export const uploadFile = async (fd: FormData, index: number) => {
+  return await service.request({
+    url: '/upload',
+    method: 'post',
+    data: fd,
+    onUploadProgress: (progressEvent) => {
+      console.log('index: ', index);
+      console.log(progressEvent);
+      console.log("progress: ", progressEvent.loaded / progressEvent.total!);
+    }
+  })
+  // return await service.post('/upload', fd, {});
   // return await service.post('/upload', { data: fd });
+}
+
+export const mergeChunks = async (data: { fileName: string, fileHash: string }) => {
+  return await service.post('/merge', data);
 }
